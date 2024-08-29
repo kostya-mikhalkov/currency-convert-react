577a21cb10169dda96f2eb46703755dfe8812325
Konstantin <kostya.mikhalkov@bk.ru>
2024-08-29 11:24:31 +0300

    feat: added last functionality

--- src/index.js
+++ src/index.js
@@ -16,22 +16,3 @@ root.render(
   </React.StrictMode>
 );
 
-
-export const request = async (currency, method, body = null, headers = {
-  'Content-Type': 'application/json',
-}) => {
-  try {
-    const req = await fetch(`https://v6.exchangerate-api.com/v6/6aa0610586124845bd8e7391/latest/${currency}`, { method, body, headers });
-    if (!req.ok) {
-      throw new Error(`HTTP error! status: ${req.status}`);
-    }
-    const resp = await req.json();
-    return resp;
-  } catch (e) {
-    throw new Error(e.message);
-  }
-}
-
-request('RUB')
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
